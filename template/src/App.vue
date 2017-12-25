f7dd10e410d7c1b7ad0f66c153a1edf759bcb180
chenguanbin <chenguanbin@hikvision.com.cn>
2017-12-25 19:14:30 +0800

    更新App.vue，上传换肤功能

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <page :menu="menu">
+    <div class="page-loading" v-loading.body="loading" v-show="loading"></div>
     <transition name="page-transition">
-        <router-view></router-view>
+      <router-view></router-view>
     </transition>
   </page>
 </template>
 
 <script>
 import navList from './nav.config.json'
+import skinJson from '../static/skin/skin.json'
 export default {
   name: 'app',
   data () {
     return {
-      menu: navList
+      menu: navList,
+      loading: true
     }
+  },
+  created () {
+    // 读取默认皮肤
+    let skin = skinJson.default
+    this.$utils.renderSkin(skin, skinJson.packages)
+  },
+  mounted () {
+    // 默认加载loading效果，800ms后移除
+    setTimeout(() => {
+      this.loading = false
+    }, 800)
   }
 }
 </script>
